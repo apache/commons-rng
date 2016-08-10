89a2df072ca4538dea50ede643a1d1013232044f
Gilles <erans@apache.org>
2016-08-10 20:36:35 +0200

    JMH benchmarks.
    Thanks to Artem Barger.

--- src/test/java/org/apache/commons/rng/ComputePiTestPerformance.java
+++ src/test/java/org/apache/commons/rng/ComputePiTestPerformance.java
@@ -24,8 +24,8 @@ import java.util.concurrent.TimeUnit;
  * by running a simple workload: computation of \( \pi \).
  *
  * The computation estimates the value by computing the probability that
- * a point p=(x, y) will lie in the circle of radius \( r = 1 \) inscribed
- * in the square.
+ * a point \( p = (x, y) \) will lie in the circle of radius \( r = 1 \)
+ * inscribed in the square of side \( r = 1 \).
  * The probability could be computed by \[ area_{circle} / area_{square} \],
  * where \( area_{circle} = \pi * r^2 \) and \( area_{square} = 4 r^2 \).
  * Hence, the probability is \( \frac{\pi}{4} \).
@@ -39,49 +39,18 @@ import java.util.concurrent.TimeUnit;
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 @Fork(1)
-public class ComputePiTestPerformance {
+@State(Scope.Benchmark)
+public class ComputePiTestPerformance extends AbstractTestPerformance {
     /**
-     * The benchmark state (retrieve the various "RandomSource"s).
+     * Number of 2D-points to generate.
      */
-    @State(Scope.Benchmark)
-    public static class Sources {
-        /**
-         * RNG providers.
-         */
-        @Param({"JDK",
-                "WELL_512_A",
-                "WELL_1024_A",
-                "WELL_19937_A",
-                "WELL_19937_C",
-                "WELL_44497_A",
-                "WELL_44497_B",
-                "MT",
-                "ISAAC",
-                "SPLIT_MIX_64",
-                "XOR_SHIFT_1024_S",
-                "TWO_CMRES",
-                "MT_64" })
-                String randomSourceName;
-
-        /**
-         * Number of 2D-points to generate.
-         */
-        @Param({"1000000"})
-        long numPoints;
-        
-        UniformRandomProvider provider;
-
-        @Setup
-        public void setup() {
-            final RandomSource randomSource = RandomSource.valueOf(randomSourceName);
-            provider = RandomSource.create(randomSource);
-        }
-    }
+    @Param({"5000000"})
+    long numPoints;
 
     @Benchmark
     public double computePi(Sources data) {
         long numPointsInCircle = 0;
-        for (int i = 0; i < data.numPoints; i++) {
+        for (int i = 0; i < numPoints; i++) {
             final double x = data.provider.nextDouble();
             final double y = data.provider.nextDouble();
             final double r2 = x * x + y * y;
@@ -90,8 +59,8 @@ public class ComputePiTestPerformance {
             }
         }
 
-        final double pi = 4 * numPointsInCircle / (double) data.numPoints;
-        // System.out.println("pi=" + pi);
+        final double pi = 4 * numPointsInCircle / (double) numPoints;
+        System.out.println("pi=" + pi);
         return pi;
     }
 }
