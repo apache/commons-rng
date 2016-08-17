74bca4e1d0e72e90e22d837ad8d93dc9a8d58a8b
Gilles <erans@apache.org>
2016-08-17 20:11:54 +0200

    Removed duplicate code.

--- src/main/java/org/apache/commons/rng/internal/source32/Well44497b.java
+++ src/main/java/org/apache/commons/rng/internal/source32/Well44497b.java
@@ -32,57 +32,23 @@ package org.apache.commons.rng.internal.source32;
  * @see <a href="http://www.iro.umontreal.ca/~panneton/WELLRNG.html">WELL Random number generator</a>
  * @since 1.0
  */
-public class Well44497b extends AbstractWell {
-    /** Number of bits in the pool. */
-    private static final int K = 44497;
-    /** First parameter of the algorithm. */
-    private static final int M1 = 23;
-    /** Second parameter of the algorithm. */
-    private static final int M2 = 481;
-    /** Third parameter of the algorithm. */
-    private static final int M3 = 229;
-    /** The indirection index table. */
-    private static final IndexTable TABLE = new IndexTable(K, M1, M2, M3);
-
+public class Well44497b extends Well44497a {
     /**
      * Creates a new random number generator.
      *
      * @param seed Initial seed.
      */
     public Well44497b(int[] seed) {
-        super(K, seed);
+        super(seed);
     }
 
     /** {@inheritDoc} */
     @Override
     public int next() {
-        // compute raw value given by WELL44497a generator
-        // which is NOT maximally-equidistributed
-        final int indexRm1 = TABLE.getIndexPred(index);
-        final int indexRm2 = TABLE.getIndexPred2(index);
+        int z4 = super.next();
 
-        final int v0 = v[index];
-        final int vM1 = v[TABLE.getIndexM1(index)];
-        final int vM2 = v[TABLE.getIndexM2(index)];
-        final int vM3 = v[TABLE.getIndexM3(index)];
-
-        // the values below include the errata of the original article
-        final int z0 = (0xFFFF8000 & v[indexRm1]) ^ (0x00007FFF & v[indexRm2]);
-        final int z1 = (v0 ^ (v0 << 24))  ^ (vM1 ^ (vM1 >>> 30));
-        final int z2 = (vM2 ^ (vM2 << 10)) ^ (vM3 << 26);
-        final int z3 = z1 ^ z2;
-        final int z2Prime = ((z2 << 9) ^ (z2 >>> 23)) & 0xfbffffff;
-        final int z2Second = ((z2 & 0x00020000) != 0) ? (z2Prime ^ 0xb729fcec) : z2Prime;
-        int z4 = z0 ^ (z1 ^ (z1 >>> 20)) ^ z2Second ^ z3;
-
-        v[index] = z3;
-        v[indexRm1] = z4;
-        v[indexRm2] &= 0xFFFF8000;
-        index = indexRm1;
-
-        // add Matsumoto-Kurita tempering
-        // to get a maximally-equidistributed generator
-        z4 ^= (z4 <<  7) & 0x93dd1400;
+        // Matsumoto-Kurita tempering to get a maximally equidistributed generator.
+        z4 ^= (z4 << 7) & 0x93dd1400;
         z4 ^= (z4 << 15) & 0xfa118000;
 
         return z4;
