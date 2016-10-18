apache/commons-rng

commit fd22dc61fd0ea75563ed893ad4857595de58a5dc

Moved ProviderBuilder to the base package and made it package private

src/main/java/org/apache/commons/rng/RandomSource.java
@@ -17,7 +17,7 @@
 package org.apache.commons.rng;
 
 import java.util.Arrays;
-import org.apache.commons.rng.internal.ProviderBuilder;
+
 import org.apache.commons.rng.internal.BaseProvider;
 import org.apache.commons.rng.internal.util.SeedFactory;
 

src/main/java/org/apache/commons/rng/internal/package-info.java
@@ -43,7 +43,7 @@
  *
  * <p>
  * Each implementation must have an identifier in
- * {@link org.apache.commons.rng.internal.ProviderBuilder.RandomSourceInternal}
+ * {@link org.apache.commons.rng.ProviderBuilder.RandomSourceInternal}
  * which must be referred to from the {@link org.apache.commons.rng.RandomSource public API}.
  * </p>
  */

src/main/java/org/apache/commons/rng/ProviderBuilder.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.rng.internal;
+package org.apache.commons.rng;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +24,6 @@ import java.util.HashMap;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
-import org.apache.commons.rng.UniformRandomProvider;
 import org.apache.commons.rng.internal.util.SeedFactory;
 import org.apache.commons.rng.internal.util.NoOpConverter;
 import org.apache.commons.rng.internal.util.Int2Long;
@@ -62,7 +61,7 @@ import org.apache.commons.rng.internal.source64.MersenneTwister64;
  * and performs seed type conversions.
  * </p>
  */
-public class ProviderBuilder {
+class ProviderBuilder {
     /** Error message. */
     private static final String INTERNAL_ERROR_MSG = "Internal error: Please file a bug report";
     /** Length of the seed array (for random seed). */

commit a6b2a5b77e5d52339ecf65bb4b1b042454ba8991

Manual revert.
As discussed on the ML, commit fd22dc61fd0ea75563ed893ad4857595de58a5dc
broke the generated "apidocs" (containing notes about user API and about
classes which contributors might need to touch).

There is no gain in hiding this one in particular, where others are also
"internal" but cannot be hidden in that way (short of putting them all
in the same package).
Moreover, it is possible that some usage might require access to the
"internal" classes.  In any case, classes within the "internal" package
are subject to change without notice, and application developers who want
to fiddle with them have been warned.

src/main/java/org/apache/commons/rng/RandomSource.java
@@ -18,6 +18,7 @@ package org.apache.commons.rng;
 
 import java.util.Arrays;
 
+import org.apache.commons.rng.internal.ProviderBuilder;
 import org.apache.commons.rng.internal.util.SeedFactory;
 
 /**

src/main/java/org/apache/commons/rng/internal/ProviderBuilder.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.rng;
+package org.apache.commons.rng.internal;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,6 +24,8 @@ import java.util.HashMap;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
+import org.apache.commons.rng.UniformRandomProvider;
+import org.apache.commons.rng.RestorableUniformRandomProvider;
 import org.apache.commons.rng.internal.util.SeedFactory;
 import org.apache.commons.rng.internal.util.NoOpConverter;
 import org.apache.commons.rng.internal.util.Int2Long;
@@ -61,7 +63,7 @@ import org.apache.commons.rng.internal.source64.MersenneTwister64;
  * and performs seed type conversions.
  * </p>
  */
-class ProviderBuilder {
+public class ProviderBuilder {
     /** Error message. */
     private static final String INTERNAL_ERROR_MSG = "Internal error: Please file a bug report";
     /** Length of the seed array (for random seed). */

src/main/java/org/apache/commons/rng/internal/package-info.java
@@ -43,8 +43,9 @@
  *
  * <p>
  * Each implementation must have an identifier in
- * {@link org.apache.commons.rng.ProviderBuilder.RandomSourceInternal}
- * which must be referred to from the {@link org.apache.commons.rng.RandomSource public API}.
+ * {@link org.apache.commons.rng.internal.ProviderBuilder.RandomSourceInternal
+ * ProviderBuilder.RandomSourceInternal} which must be referred to from the
+ * {@link org.apache.commons.rng.RandomSource public API}.
  * </p>
  */
 