aa246979feb8c880c60c972faf7c9ffb9174f4cd
Alex Herbert <aherbert@apache.org>
2019-05-31 22:35:25 +0100

    RNG-75: Use SplitMix64.next()

--- commons-rng-simple/src/main/java/org/apache/commons/rng/simple/internal/Long2IntArray.java
+++ commons-rng-simple/src/main/java/org/apache/commons/rng/simple/internal/Long2IntArray.java
@@ -62,11 +62,11 @@ public class Long2IntArray implements Seed2ArrayConverter<Long, int[]> {
         int i = 0;
         // Handle an odd size
         if ((size & 1) == 1) {
-            out[i++] = NumberFactory.extractHi(rng.nextLong());
+            out[i++] = NumberFactory.extractHi(rng.next());
         }
         // Fill the remaining pairs
         while (i < size) {
-            final long v = rng.nextLong();
+            final long v = rng.next();
             out[i] = NumberFactory.extractHi(v);
             out[i + 1] = NumberFactory.extractLo(v);
             i += 2;
