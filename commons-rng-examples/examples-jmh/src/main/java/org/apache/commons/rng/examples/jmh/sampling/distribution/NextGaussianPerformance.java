f328057b5fd46a3a2b3d7afc6fa2ada134c6bc97
Alex Herbert <aherbert@apache.org>
2021-07-26 13:47:52 +0100

    Add the NormalizedGaussianSamplers to  NextGaussianPerformance benchmark
    This allows the report table in the user manual to be generated from a
    single JMH benchmark.

--- commons-rng-examples/examples-jmh/src/main/java/org/apache/commons/rng/examples/jmh/sampling/distribution/NextGaussianPerformance.java
+++ commons-rng-examples/examples-jmh/src/main/java/org/apache/commons/rng/examples/jmh/sampling/distribution/NextGaussianPerformance.java
@@ -17,6 +17,13 @@
 
 package org.apache.commons.rng.examples.jmh.sampling.distribution;
 
+import org.apache.commons.rng.UniformRandomProvider;
+import org.apache.commons.rng.examples.jmh.RandomSources;
+import org.apache.commons.rng.sampling.distribution.BoxMullerNormalizedGaussianSampler;
+import org.apache.commons.rng.sampling.distribution.MarsagliaNormalizedGaussianSampler;
+import org.apache.commons.rng.sampling.distribution.NormalizedGaussianSampler;
+import org.apache.commons.rng.sampling.distribution.ZigguratNormalizedGaussianSampler;
+import org.apache.commons.rng.sampling.distribution.ZigguratSampler;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
@@ -25,13 +32,16 @@ import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import java.util.concurrent.TimeUnit;
 import java.util.Random;
 
 /**
- * Benchmark for {@link Random#nextGaussian()} in order to compare
- * the speed of generation of normally-distributed random numbers.
+ * Benchmark to compare the speed of generation of normally-distributed random
+ * numbers of {@link Random#nextGaussian()} against implementations of
+ * {@link NormalizedGaussianSampler}.
  */
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -40,15 +50,80 @@ import java.util.Random;
 @State(Scope.Benchmark)
 @Fork(value = 1, jvmArgs = {"-server", "-Xms128M", "-Xmx128M"})
 public class NextGaussianPerformance {
-    /** JDK's generator. */
-    private final Random random = new Random();
-
     /**
      * The value.
      *
      * <p>This must NOT be final!</p>
      */
     private double value;
+
+    /**
+     * The {@link Random} to use for testing.
+     * This uses a non-final instance created for the benchmark to make a fair comparison
+     * to the other Gaussian samplers by using the same setup and sampler access.
+     */
+    @State(Scope.Benchmark)
+    public static class JDKSource {
+        /** JDK's generator. */
+        private Random random;
+
+        /**
+         * @return the sampler.
+         */
+        public Random getSampler() {
+            return random;
+        }
+
+        /** Instantiates sampler. */
+        @Setup
+        public void setup() {
+            random = new Random();
+        }
+    }
+
+    /**
+     * The {@link NormalizedGaussianSampler} samplers to use for testing. Creates the sampler for each
+     * {@link org.apache.commons.rng.simple.RandomSource RandomSource} in the default
+     * {@link RandomSources}.
+     */
+    @State(Scope.Benchmark)
+    public static class Sources extends RandomSources {
+        /** The sampler type. */
+        @Param({"BoxMullerNormalizedGaussianSampler",
+                "MarsagliaNormalizedGaussianSampler",
+                "ZigguratNormalizedGaussianSampler",
+                "ZigguratSampler.NormalizedGaussian"})
+        private String samplerType;
+
+        /** The sampler. */
+        private NormalizedGaussianSampler sampler;
+
+        /**
+         * @return the sampler.
+         */
+        public NormalizedGaussianSampler getSampler() {
+            return sampler;
+        }
+
+        /** Instantiates sampler. */
+        @Override
+        @Setup
+        public void setup() {
+            super.setup();
+            final UniformRandomProvider rng = getGenerator();
+            if ("BoxMullerNormalizedGaussianSampler".equals(samplerType)) {
+                sampler = BoxMullerNormalizedGaussianSampler.of(rng);
+            } else if ("MarsagliaNormalizedGaussianSampler".equals(samplerType)) {
+                sampler = MarsagliaNormalizedGaussianSampler.of(rng);
+            } else if ("ZigguratNormalizedGaussianSampler".equals(samplerType)) {
+                sampler = ZigguratNormalizedGaussianSampler.of(rng);
+            } else if ("ZigguratSampler.NormalizedGaussian".equals(samplerType)) {
+                sampler = ZigguratSampler.NormalizedGaussian.of(rng);
+            } else {
+                throw new IllegalStateException("Unknown sampler type: " + samplerType);
+            }
+        }
+    }
 
     /**
      * Baseline for the JMH timing overhead for production of an {@code double} value.
@@ -61,12 +136,24 @@ public class NextGaussianPerformance {
     }
 
     /**
-     * Run JDK random gaussian sampler.
+     * Run JDK {@link Random} gaussian sampler.
      *
-     * @return the double
+     * @param source Source of randomness.
+     * @return the sample value
      */
     @Benchmark
-    public double runJDKRandomGaussianSampler() {
-        return random.nextGaussian();
+    public double sampleJDK(JDKSource source) {
+        return source.getSampler().nextGaussian();
+    }
+
+    /**
+     * Run the {@link NormalizedGaussianSampler} sampler.
+     *
+     * @param sources Source of randomness.
+     * @return the sample value
+     */
+    @Benchmark
+    public double sample(Sources sources) {
+        return sources.getSampler().sample();
     }
 }
