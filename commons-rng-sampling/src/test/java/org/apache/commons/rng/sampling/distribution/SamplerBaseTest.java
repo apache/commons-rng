f5f958c7a2eec722a828a5fcb7acad3c2cf52c62
Alex Herbert <aherbert@apache.org>
2021-09-18 08:05:19 +0100

    Directly test the SamplerBase class
    There is no requirement to extend the class to access the pass-through
    methods.

--- commons-rng-sampling/src/test/java/org/apache/commons/rng/sampling/distribution/SamplerBaseTest.java
+++ commons-rng-sampling/src/test/java/org/apache/commons/rng/sampling/distribution/SamplerBaseTest.java
@@ -25,42 +25,13 @@ import org.junit.jupiter.api.Test;
  * Test for the {@link SamplerBase}. The class is deprecated but is public. The methods
  * should be tested to ensure correct functionality.
  */
+@SuppressWarnings("deprecation")
 class SamplerBaseTest {
-    /**
-     * Extends the {@link SamplerBase} to allow access to methods.
-     */
-    @SuppressWarnings("deprecation")
-    private static class SimpleSampler extends SamplerBase {
-        SimpleSampler(UniformRandomProvider rng) {
-            super(rng);
-        }
-
-        @Override
-        public double nextDouble() {
-            return super.nextDouble();
-        }
-
-        @Override
-        public int nextInt() {
-            return super.nextInt();
-        }
-
-        @Override
-        public int nextInt(int max) {
-            return super.nextInt(max);
-        }
-
-        @Override
-        public long nextLong() {
-            return super.nextLong();
-        }
-    }
-
     @Test
     void testNextMethods() {
         final UniformRandomProvider rng1 = RandomSource.SPLIT_MIX_64.create(0L);
         final UniformRandomProvider rng2 = RandomSource.SPLIT_MIX_64.create(0L);
-        final SimpleSampler sampler = new SimpleSampler(rng2);
+        final SamplerBase sampler = new SamplerBase(rng2);
         final int n = 256;
         for (int i = 0; i < 3; i++) {
             Assertions.assertEquals(rng1.nextDouble(), sampler.nextDouble());
@@ -73,7 +44,7 @@ class SamplerBaseTest {
     @Test
     void testToString() {
         final UniformRandomProvider rng = RandomSource.SPLIT_MIX_64.create(0L);
-        final SimpleSampler sampler = new SimpleSampler(rng);
+        final SamplerBase sampler = new SamplerBase(rng);
         Assertions.assertTrue(sampler.toString().contains("rng"));
     }
 }
